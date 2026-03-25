4a2943b2f343a517206dd9dcef064e569155d04b
Joao <jho_jbbantonio11@live.com>
2026-03-24 21:51:30 -0300

    Subindo Còdigo!

---
+++ src/services/password/create.js
@@ -0,0 +1,12 @@
+import chalk from "chalk"
+import handle from "./handle.js"
+
+
+async function createPassword(){
+    const password = await handle();
+    console.log(password);
+}
+
+
+
+export default createPassword
